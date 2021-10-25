ad45bfb0ce4691e6029f85e668d71867f1bc7f7e
tu201906038 <tu201906038@215-7>
2021-10-25 17:33:32 +0400

    task

--- 10-25-2021/Program.cs
+++ 10-25-2021/Program.cs
@@ -6,14 +6,65 @@ namespace _10_25_2021
     {
         static void Main(string[] args)
         {
-            //yaz - 3 4 5
-            //yay - 6 7 8
-            //payis - 9 10 11 
-            // qis- 12 1 2
+
+            while (true)
+            {
+                int input = Convert.ToInt32(Console.ReadLine());
+                int count = 2;
+                int eded = 2;
+                bool sadedimi = true;
+                while (eded < input)
+                {
+                    if (input % eded == 0)
+                    {
+                        for (int i = 2; i * i <= eded; i++)
+                        {
+                            if (eded % i == 0)
+                            {
+                                sadedimi = false;
+
+                            }
+
+                        }
+
+                        count++;
+                    }
+                    eded++;
+
+                }
+                
+
+            }
+            #region task bolenleri
+            /**
+             int input = Convert.ToInt32(Console.ReadLine());
+            int count = 2;
+            int eded = 2;
+            while (eded < input)
+            {
+                if (input % eded == 0)
+                {
+                    count++;
+                }
+                eded++;
+
+            }
+            Console.WriteLine(count);
+            **/
+            #endregion
+
 
 
             #region Fesiller
-
+            /**
+             * 
+             * //yaz - 3 4 5
+            //yay - 6 7 8
+            //payis - 9 10 11 
+            // qis- 12 1 2
+            
+             * 
+             * 
             while (true)
             {
                 try
@@ -52,6 +103,7 @@ namespace _10_25_2021
                     Console.WriteLine("duzgun formatda daxil edin");
                 }
             }
+            **/
             #endregion
         }
     }
